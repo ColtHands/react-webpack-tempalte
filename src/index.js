b6418303b1a552c3650571d0dd0f84c56639d64a
Aleskey.K <f1ks3r@yandex.ru>
2022-04-24 19:09:05 +0300

    save

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import { createRoot } from 'react-dom/client'
 import React from 'react'
 import { App } from '@/App.tsx'
 
-console.log('hello world')
-
 createRoot(document.getElementById('app-root'))
     .render(<App/>)
